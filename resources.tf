e33a62169176f8d9af5a190c92bb0dcf316f2174
William Gatharia <wgatharia@yahoo.com>
2019-12-01 16:24:55 -0800

    added azure network

--- resources.tf
+++ resources.tf
@@ -4,7 +4,29 @@
 # }
 
 
-resource "azurerm_resource_group" "our_development_resource" {
-    name="testResourceGroup1"
+resource "azurerm_resource_group" "our_development_network" {
+    name="devresgrp"
     location="West US"
+}
+
+resource "azurerm_virtual_network" "blue_virtual_network" {
+  address_space = ["10.0.0.0/16"]
+  location="West US"
+  name="bluevirtnetwork"
+  resource_group_name="${azurerm_resource_group.our_development_network.name}"
+  dns_servers=["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+      name="subnet1"
+      address_prefix="10.0.1.0/24"
+  }
+
+  subnet {
+      name="subnet2"
+      address_prefix="10.0.2.0/24"
+  }
+
+  tags={
+      environment="blue-world-finder"
+  }
 }
